8d5d84e4c139a2e36fb66bf67dd4eef61d41e50e
Binni Zenobio Cordova Leandro <u08849@PC1880.DOMINIO>
2018-08-17 13:48:25 +0200

    inizio per la configurazione degli utenti e la tabella "alumnos"

---
+++ config/trentino.sql
@@ -0,0 +1,46 @@
+/* CONFIGURACION:
+ * el nombre de la tabla sera impuesta en  plural y con el prefijo "t_".
+ * los nombres de las claves primarias para las tablas seran en singular y con el prefijo "pk_".
+ * las tablas de tipo relacion tendran el prefijo "rel_" y el nombre de las tablas relacionadas separadas por "_"
+ * los nombre de las claves externas seran llamadas junto al prefijo "fk_" y el nombre de la llave en singular
+*/
+
+create table trentino.t_usuarios(
+	dni char(8) not null,
+	contrasegna varchar(200) not null,
+	email varchar(200) not null,
+	suscriptcion date not null,
+	activo boolean default false,
+	constraint pk_usuario primary key(dni)
+)
+
+create table trentino.t_roles(
+	id integer not null,
+	nombre varchar(50) not null,
+	descripcion varchar(200),
+	constraint pk_rol primary key(id)
+)
+
+create table trentino.t_funcionalidades(
+	id integer not null,
+	nombre varchar(50) not null,
+	descripcion varchar(200),
+	constraint pk_funcionalidad primary key(id)
+)
+
+create table trentino.rel_roles_funcionalidades(
+  id_rol integer not null,
+  id_funcionalidad integer not null,
+  constraint pk_rol_funcionalidad primary key (id_rol,id_funcionalidad),
+  constraint fk_rol foreign key(id_rol) references trentino.t_roles(id),
+  constraint fk_funcionalidad foreign key(id_funcionalidad) references trentino.t_funcionalidades(id)
+)
+
+create table trentino.t_alumnos(
+	id integer not null,
+	dni char(8) default null,
+	apellidos varchar(100),
+	nombres varchar(100),
+	nacimiento date,
+	constraint pk_users primary key (id)
+)
